3566b0ee8db32f128813065fc2f7061d41e7acf9
marcelo <marcelo@manguaba>
2013-04-23 15:24:45 -0300

    First Version

---
+++ src/com/marcelodamasceno/util/Utils.java
@@ -0,0 +1,16 @@
+package com.marcelodamasceno.util;
+
+import java.util.Iterator;
+
+import weka.core.Instances;
+
+public class Utils {
+	
+	public Instances mergeDataSets(Instances data1, Instances data2){
+		for(int instance=0;instance<data2.numInstances();instance++){
+			data1.add(data2.instance(instance));
+		}
+		return data1;
+	}
+
+}
